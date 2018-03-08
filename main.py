d3586b5ef531df713c12d8ca5ddfbecf6ea007df
avoss19 <avoss19@bsmschool.org>
2018-03-08 16:19:14 -0600

    fixed ip print & made similar changes to kitBotMain

--- main.py
+++ main.py
@@ -16,28 +16,20 @@ Not tested on windows
 
 Notes:
 - Tasks https://goo.gl/57n8XC & https://github.com/avoss19/RoboGUI
-- switch QDialog to QMainWindow (line 30)
-    - Fix size of widgets use setGeometry/geometry or rezise
-- Camera stream https://gist.github.com/cms-/1cd8ff5083884a4355bd65f084eda927
 '''
 
 import sys, os, time
+import camera
 import ip
-from PyQt4.QtCore import SIGNAL, QThread, pyqtSlot, QSize, QTimer
+from PyQt4.QtCore import SIGNAL, QThread, pyqtSlot, QSize, QTimer, QUrl
 from PyQt4.QtGui import *
-from PyQt4.QtWebKit import *
+
 
 class Form(QWidget):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
         self.initScreenSize() # If window mode set to fullscreen window
-
-        self.setMinimumSize(800, 600)
-
-        # window size and position
-        self.resize(self.x,self.y) # setFixedSize disables window resizing
-        self.move(0,0)
 
         self.pyQtResolution() # Get window resolution
 
@@ -49,33 +41,24 @@ class Form(QWidget):
         # window title
         self.setWindowTitle("RoboGUI")
 
+        # Display widgets
+        self.videoStream()
         self.printIP()
-        self.pic0()
         self.controllerButton()
-        self.changePic()
         self.quitButton()
 
-        self.debugButton()
-
+        self.debugButton() # For testing purposes
         #self.updateDisplayB()
 
 
-    def updateDisplay(self):
-        self.pyQtResolution()
-
-        self.debugB.move(0,self.y-30)
-        self.ip.move(self.x - 200,5)
-
-        self.update()
-
-
-    def updateDisplayB(self):
-        self.displayB = QPushButton("Update Display", self)
-        self.displayB.move(0,400)
-        self.connect(self.displayB, SIGNAL("clicked()"),self.updateDisplay)
+    def initScreenSize(self):
+        # For fullscreen app
+        screen_resolution = app.desktop().screenGeometry()
+        self.x, self.y = screen_resolution.width(), screen_resolution.height()
 
 
     def debug(self):
+        # For printing info to console
         #print sys.argv
         print "PyQt4 Resolution: ", self.x, " x ", self.y
         print self.size()
@@ -93,60 +76,74 @@ class Form(QWidget):
         self.connect(self.debugB, SIGNAL("clicked()"),self.debug)
 
 
-    def initScreenSize(self):
-        screen_resolution = app.desktop().screenGeometry()
-        self.x, self.y = screen_resolution.width(), screen_resolution.height()
-
-
     def pyQtResolution(self):
+        # Get resolution of window
         rec = self.geometry()
         self.x = rec.width()
         self.y = rec.height()
 
 
+    def center(self):
+        # Center window on display
+        frameGm = self.frameGeometry()
+        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+        centerPoint = QApplication.desktop().screenGeometry(screen).center()
+        frameGm.moveCenter(centerPoint)
+        self.move(frameGm.topLeft())
+
+
+    def updateDisplay(self):
+        self.pyQtResolution()
+
+        self.debugB.move(0,self.y-30)
+        self.ip.move(self.x - 200,5)
+        self.ip.resize(195, self.y - 10)
+
+        self.update()
+
+
+    def updateDisplayB(self):
+        self.displayB = QPushButton("Update Display", self)
+        self.displayB.move(0,400)
+        self.connect(self.displayB, SIGNAL("clicked()"),self.updateDisplay)
+
+
+
+    def videoStream(self):
+        # Video Feed
+        self.layout = QGridLayout(self)
+        view = camera.camera(ip.camera1)
+
+        # set size of camera stream
+        view.setMaximumHeight(500)
+        view.setMaximumWidth(500)
+
+        # Add widget to display
+        self.layout.addWidget(view)
+
+
     def printIP(self):
+        # Print connected ip addresses to screen
         ipAddresses = "<b>Connected IP Addresses:</b>"
         nextLine = "<br />"
+        numIP = 0
 
         # get ip address from ip.py
         for i in ip.ips:
             if i[:1] != "_":
                 exec("ipAddresses = ipAddresses + nextLine + i + ' = ' + ip." + str(i))
+                numIP = numIP + 1
 
         # change color of ip address text
         ipAddresses = "<font color ='green'>" + ipAddresses + "</font>"
 
         # Create text on screen to display ip addresses
-        self.ip = QLabel(ipAddresses, self)
+        self.ip = QTextEdit(self)
+        self.ip.insertHtml(ipAddresses)
+        self.ip.setReadOnly(True)
 
-        # position of ip address text
-        self.ip.move(self.x - 200,5)
-
-
-    def pic0(self):
-        self.picName = QLabel("<b>Camera1</b>", self)
-        self.picName.move(5, 85)
-
-        self.pic = QLabel(self)
-        self.pic.setPixmap(QPixmap("pepe0.jpg"))
-
-        # http image
-        #self.pic = QWebView(self)
-        #self.pic.setHtml("<img src='http://" + ip.camera1 + ":8080?action=stream" + "' />") # robot video
-        #self.pic.setHtml("<img src='http://localhost/pepe0.jpg' />") # test image
-
-        self.pic.move(5,100)
-
-
-    def pic1(self):
-        # doesn't work with web pic
-        self.pic.setPixmap(QPixmap("pepe1.jpg"))
-
-
-    def controllerButton(self):
-        self.controller = QPushButton("Run Controller-Support", self)
-        self.controller.move(0,5)
-        self.connect(self.controller, SIGNAL("clicked()"),self.controllerStart)
+        # position of ip address text & resize
+        self.ip.move(self.x - 200, 5)
 
 
     def controllerStart(self):
@@ -154,34 +151,37 @@ class Form(QWidget):
         os.system("python Controller.py &")
 
 
-    def changePic(self):
-        self.test = QPushButton("Change Pic", self)
-        self.test.move(0,30)
-        self.connect(self.test, SIGNAL("clicked()"),self.pic1)
-
-
-    def quitButton(self):
-        self.q = QPushButton("Quit", self)
-        self.q.setStyleSheet('QPushButton {color: red;}')
-        self.q.move(0,55)
-        self.connect(self.q, SIGNAL("clicked()"),self.quit)
+    def controllerButton(self):
+        self.controller = QPushButton("Run Controller-Support", self)
+        self.controller.move(0,5)
+        self.connect(self.controller, SIGNAL("clicked()"),self.controllerStart)
+        self.controller.resize(180,30)
 
 
     def quit(self):
-        # quits all running python programs (not best method to kill program)
-        # tested on two different macs and for some reason one python task is capitalized while the other isn't
+        # quits all running python programs (not best method to kill program), but will kill controller-support
+        # Only tested on mac osx; but should technically work
         from sys import platform
 
         if platform == "linux" or platform == "linux2":
             os.system("killall -9 python")
             os.system("killall -9 Python")
 
+        # tested on two different macs and for some reason one python task is capitalized while the other isn't
+        # not sure if it is an isolated situation
         elif platform == "darwin":
             os.system("killall -9 python")
             os.system("killall -9 Python")
 
         elif platform == "win32":
             os.system("taskkill /F /IM python.exe /T")
+
+
+    def quitButton(self):
+        self.q = QPushButton("Quit", self)
+        self.q.setStyleSheet('QPushButton {color: red;}')
+        self.q.move(0,30)
+        self.connect(self.q, SIGNAL("clicked()"),self.quit)
 
 
 def windowMode():
@@ -193,20 +193,10 @@ def windowMode():
     except:
         form.showFullScreen()
 
-def main():
-    global app, form
-    os.system("clear")
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = Form()
     windowMode()
-    #form.updateDisplay() # Fixes issues with misplaced widgets in windowed mode
-
-    # Update display every second; Poor implementation
-    timer = QTimer(form)
-    timer.timeout.connect(form.updateDisplay)
-    timer.start(1)
-
-    app.exec_()
-
-if __name__ == '__main__':
-    main()
+    form.updateDisplay() # Fix miss placed widgets
+    sys.exit(app.exec_())
